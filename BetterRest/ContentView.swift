940ca8e5459b1e71ea73b0767b1b44e202e00baa
YASHAVIKA SINGH <YASHAVIKA.SINGH@GMAIL.COM>
2024-06-03 21:59:19 +0000

    basic UI with steppers and date picker

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var sleepAmount =  8.0
+    @State private var wakeUp = Date.now
+    @State private var coffeeAmount = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationStack{
+            VStack{
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .padding(40)
+                    .font(.title2)
+                    .labelsHidden()
+                
+                Text("Desired amount   of sleep ")
+                    .font(.headline)
+                
+                Stepper("Sleep Amount: \(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                    .padding(40)
+                    
+                
+                Text("Daily coffee intake?")
+                    .font(.headline)
+                Stepper("  \(coffeeAmount) Cups of coffee: ", value: $coffeeAmount, in: 0...10, step: 1)
+                    .padding(40)
+                    .navigationTitle("BettterRest")
+                        .toolbar{
+                            Button("Calculate", action: calculateBedtime)
+                        }
+                
+            }
         }
-        .padding()
+        
+     
+        
+        
+    }
+    
+    
+    func calculateBedtime(){
+         
     }
 }
 
