931b0aecf8ad4dfbf3361d6e9f97f93a0398336d
YASHAVIKA SINGH <YASHAVIKA.SINGH@GMAIL.COM>
2024-06-03 19:12:47 +0000

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Yashavika Singh on 03.06.24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
