2d3bb5ce72ff89fe1b8cedcce21a5f6e43bddbc9
YASHAVIKA SINGH <YASHAVIKA.SINGH@GMAIL.COM>
2024-06-04 17:14:27 +0000

    Changed UI and removed button

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -12,18 +12,20 @@ struct ContentView: View {
     @State private var sleepAmount =  8.0
     @State private var wakeUp = defaultWakeUpTime
     @State private var coffeeAmount = 1
-    
+    @State private var finalMessage = ""
     @State private var showingAlert = false
     @State private var alertTitle = ""
     @State private var alertMessage = ""
+
     
-    
-   static  var defaultWakeUpTime: Date {
+    static var defaultWakeUpTime: Date {
         var components = DateComponents()
         components.hour = 7
         components.minute = 0
         return Calendar.current.date(from: components ) ?? .now
     }
+    
+ 
     
     
     var body: some View {
@@ -31,52 +33,45 @@ struct ContentView: View {
         NavigationStack{
             Form{
                 
-                VStack(alignment: .leading, spacing: 0){
-                    Text("When do you want to wake up?")
-                        .font(.headline)
-                    
+                Section("When do you want to wake up?"){
+                  
                     DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                         .font(.title2)
                         .labelsHidden()
                 }
              
                 
-                VStack(alignment: .leading, spacing: 0){
-                    Text("Desired amount   of sleep ")
-                        .font(.headline)
+                Section("Desired amount   of sleep "){
+                     
                     
-                    Stepper("Sleep Amount: \(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                Stepper("Sleep Amount: \(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
                 }
-                
               
                     
                 
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Daily coffee intake?")
-                        .font(.headline)
+                Section("Daily coffee intake?") {
+                
+       
                     Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 1...20)
 
                 }
-                
-                    .navigationTitle("BettterRest")
-                    .toolbar{
-                            Button("Calculate", action: calculateBedtime)
-                        }
-                    .alert(alertTitle, isPresented: $showingAlert){
-                    Button("OK"){}
-                      } message: {
-                Text(alertMessage)
+                Section{
+                    Text( "Your ideal  bedtime is \(calculateBedtime()) ").font(.title3)
                 }
+              
+                
+                 
+                
             }
-        } 
-        
-     
-        
+            .navigationTitle("BettterRest")
+        }
+ 
         
     }
     
     
-    func calculateBedtime(){
+     func calculateBedtime() -> String{
+        var result = ""
         do{
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config )
@@ -85,16 +80,14 @@ struct ContentView: View {
             let minute = (components.minute ?? 0)*60
             let prediction = try model.prediction(wake: Double(hour+minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
             let sleepTime = wakeUp - prediction.actualSleep
-            
-            alertTitle = "Your ideal  bedtime is ..."
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        
+             result = sleepTime.formatted(date: .omitted, time: .shortened)
         }
         catch{
-          alertTitle = "Error"
-        alertMessage = "Sorry, there was a problem with the model"
+            result = "Something went wrong with the model"
         }
         
-        showingAlert = true
+        return result
     }
     
    
